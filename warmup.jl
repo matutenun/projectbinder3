5867cad378478bee77902728ec2768fd288ebc93
matutenun <matias.nunez2@gmail.com>
2021-05-17 19:00:59 -0300

    Add files via upload

--- warmup.jl
+++ warmup.jl
@@ -1,9 +1,4 @@
 using DifferentialEquations
-f(u,p,t) = 1.01*u
-u0 = 1/2
-tspan = (0.0,1.0)
-prob = ODEProblem(f,u0,tspan)
-sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8)
 using Plots
 x = 1:10
 y = rand(10) 
