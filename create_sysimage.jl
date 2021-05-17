a7d250636fed73b38ca0fd1ce7ada2c2edfa16b5
matutenun <matias.nunez2@gmail.com>
2021-05-17 19:47:46 -0300

    Add files via upload

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,5 @@
 using PackageCompiler
 create_sysimage([:DifferentialEquations, :Plots, :Pluto, :PlutoUI];
-                precompile_execution_file = "warmup.jl";
-                replace_default = true;
+                precompile_execution_file = "warmup.jl",
+                replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
